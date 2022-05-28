a82b9b9e5b24e234e0d820ca3ff7c24c4830c2d7
EC2 Default User <ec2-user@ip-172-31-12-100.ec2.internal>
2022-05-28 07:24:03 +0000

    20220528完

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -55,6 +55,6 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = @book.user
     redirect_to(books_path) unless @user == current_user
-   end
+    end
 
 end
